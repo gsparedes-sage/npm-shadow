5b44106551bae07f8075c98f9e88e2b103bbd3bc
Bruno Jouhier <bjouhier@gmail.com>
2015-09-03 15:09:58 +0200

    simplified implementation

--- index.js
+++ index.js
@@ -28,80 +28,87 @@ module.exports = function(options) {
 	var txtCacheRoot = cacheRoot;
 	var redirRoot = fsp.join(cacheRoot, "redir");
 	var varCacheRoot = fsp.join(cacheRoot, "$$BIN$$");
+	var binCacheRoot = fsp.join(cacheRoot, arch + '-' + v8);
 
-	var original = {
-		_resolveFilename: Module._resolveFilename,
-		_findPath: Module._findPath,
-	};
+	function readPackage(path) {
+		try {
+			return JSON.parse(fs.readFileSync(path, 'utf8'));
+		} catch (err) {
+			//console.error("bad package.json: " + path + ", err=" + err.message);
+		}
+	}
+
+	function copyFile(src, dstRoot) {
+		var dst = fsp.join(dstRoot, src.substring(root.length));
+		ensureDir(fsp.dirname(dst));
+		fs.writeFileSync(dst, fs.readFileSync(src));
+	}
+
+	function updateShadowModules(path, depth, pkg) {
+		fs.readdirSync(path).forEach(function(name) {
+			// don't recurse into shadow files!
+			if (name === 'shadow-modules') return;
+			var sub = fsp.join(path, name)
+			var stat = fs.lstatSync(sub);
+			if (stat.isDirectory()) {
+				log("processing " + sub);
+				var npkgPath = fsp.join(sub, 'package.json');
+				var npkg = pkg;
+				if (fs.existsSync(npkgPath)) npkg = readPackage(npkgPath);
+				var ndepth = depth;
+				if (name === 'node_modules') ndepth++;
+				updateShadowModules(sub, ndepth, npkg);
+			} else if (stat.isFile()) {
+				if ((pkg && !pkg.private) || depth >= 2) {
+					if (/\.(json|js|_js|coffee|_coffee)$/.test(name)) copyFile(sub, txtCacheRoot);
+					else if (/\.node$/.test(name)) {
+						// if already in arch subdir (fibers precompiled for ex), copy it to regular output dir
+						if (sub.indexOf(arch) >= 0) copyFile(sub, txtCacheRoot);
+						else copyFile(sub, binCacheRoot);
+					}
+				} else {
+					if (/\/fibers/.test(sub)) console.error("IGNORING " + sub, pkg && pkg.private, depth)
+				}
+			}
+		});
+	}
+
+	//updateShadowModules(root, 0, readPackage(fsp.join(root, 'package.json')));
+	//return;
 
 	function under(path, ref) {
 		return path.substring(0, ref.length) === ref;
 	}
 
-	var packages = {};
+	var original = {
+		_resolveFilename: Module._resolveFilename,
+	};
 
-	function loadPackage(name) {
-		if (packages[name] !== undefined) return packages[name];
-		var path = fsp.join(root, 'node_modules', name, 'package.json');
-		if (fs.existsSync(path)) return packages[name] = JSON.parse(fs.readFileSync(path, 'utf8'));
-		else return packages[name] = null;
-	}
-
-	function ensureCacheDir(cacheDir, foundDir) {
-		if (fs.existsSync(cacheDir)) return;
-		ensureCacheDir(fsp.join(cacheDir, '..'), fsp.join(foundDir, '..'));
-		fs.mkdirSync(cacheDir);
-		var pkgPath = fsp.join(foundDir, 'package.json');
-		if (fs.existsSync(pkgPath)) fs.writeFileSync(fsp.join(cacheDir, 'package.json'), fs.readFileSync(pkgPath));
-	}
+	var shadowPaths = [
+		fsp.join(txtCacheRoot, 'node_modules'),
+		fsp.join(binCacheRoot, 'node_modules'),
+	];
 
 	Module._resolveFilename = function(request, parent) {
-		var redir;
-		//console.error("RESOLVE" + request, parent.filename + ': paths=' + parent.paths);
-		var from = parent.filename == null ? parent.paths[0] : fsp.dirname(parent.filename);
-		if (under(from, root)) {
-			var root2 = fsp.join(root, 'node_modules');
-			var req = under(request, root2) ? request.substring(root2.length + 1) : request;
-			redir = fsp.join(from, req.replace(/[\/\\]/g, '$$'));
-			redir = redirRoot + redir.substring(root.length) + '.redir'; // .redir ext to avoid conflict with existing dires
-		}
 		try {
-			var foundPath = original._resolveFilename(request, parent);
-			if (redir && under(foundPath, root)) {
-				var binary = /\.node$/.test(foundPath) && foundPath.indexOf(arch) < 0;
-				var foundRel = foundPath.substring(root.length);
-				var comps = foundRel.split(/[\/\\]/);
-				if (comps[1] !== 'node_modules') return foundPath;
-				var pkg = loadPackage(comps[2]);
-				if (!binary && (!pkg || pkg.private)) return foundPath;
-				var cachePath = (binary ? varCacheRoot : txtCacheRoot) + foundRel;
-				var cached;
-				if (fs.existsSync(redir)) {
-					cached = JSON.parse(fs.readFileSync(redir, "utf8"));
-					if (pkg && cached.version !== pkg.version) cached = null;
-				}
-				if (!cached) {
-					ensureDir(fsp.dirname(redir));
-					fs.writeFileSync(redir, JSON.stringify({
-						version: pkg && pkg.version,
-						path: cachePath.substring(root.length),
-					}, null, '\t'), "utf8");
-				}
-				if (binary) cachePath = cachePath.replace('$$BIN$$', arch + '-' + v8);
-				if (!cached || !fs.existsSync(cachePath)) {
-					verbose && log("creating " + cachePath);
-					ensureCacheDir(fsp.dirname(cachePath), fsp.dirname(foundPath));
-					fs.writeFileSync(cachePath, fs.readFileSync(foundPath));
-				}
-			}
-			return foundPath;
+			return original._resolveFilename(request, parent);
 		} catch (err) {
-			//console.error("ERROR! ", err.stack);
-			if (err.code === 'MODULE_NOT_FOUND' && redir && fs.existsSync(redir)) {
-				var cached = JSON.parse(fs.readFileSync(redir, "utf8"));
-				cachePath = root + cached.path.replace('$$BIN$$', arch + '-' + v8);
-				return cachePath;
-			} else throw err;
+			if (err.code !== 'MODULE_NOT_FOUND') throw err;
+			//console.error("RESOLVE " + request);
+			var from = parent.filename == null ? parent.paths[0] : fsp.dirname(parent.filename);
+			if (!under(from, root)) throw err;
+			var mod = new Module(parent.id);
+			mod.filename = fsp.join(txtCacheRoot, from.substring(root.length));
+			mod.paths = shadowPaths;
+			//console.error("trying from ", mod.filename);
+			try {
+				return original._resolveFilename(request, mod);
+			} catch (err) {
+				if (err.code !== 'MODULE_NOT_FOUND') throw err;
+				mod.filename = fsp.join(binCacheRoot, from.substring(root.length));
+				//console.error("trying binary from ", mod.filename);
+				return original._resolveFilename(request, mod);
+			}
 		}
 	}
 }
